b70b68db6230374ff7c82481bff7993402d02999
Mike Cayan <mahirturan9097@gmail.com>
2026-09-10 21:09:02 +0200

    Add Azure infrastructure foundation

---
+++ infrastructure/main.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "main" {
+  name     = "rg-ai-ops-dev"
+  location = "Germany West Central"
+}
+
+resource "azurerm_container_registry" "main" {
+  name                = "acraiopsmikedev"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
