1e63ebbadcc5b6e18e884bb79927a5f7bea0e6f7
Mike Cayan <mahirturan9097@gmail.com>
2026-09-12 16:39:11 +0200

    Add deployment outputs for Container Apps

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -2,3 +2,20 @@ output "acr_login_server" {
   description = "Login server of the Azure Container Registry"
   value       = azurerm_container_registry.main.login_server
 }
+
+
+output "managed_identity_id" {
+  description = "Resource ID of the managed identity"
+  value       = azurerm_user_assigned_identity.main.id
+}
+
+output "container_app_environment_id" {
+  description = "Resource ID of the Azure Container App Environment"
+  value       = azurerm_container_app_environment.main.id
+}
+
+output "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  value       = azurerm_resource_group.main.name
+}
+
